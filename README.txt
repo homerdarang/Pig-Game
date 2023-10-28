2698f13a0adaa12345d59bb96cbb46d4d40b96e6
Homer Darang <homerdarang03@gmail.com>
2023-10-28 14:56:46 +0800

    added readme

---
+++ README.txt
@@ -0,0 +1,22 @@
+   SET current score to 0 
+   SET Total score to 0
+    User roll the dice (button)
+    SET number generate numbers 1 - 6
+    Display dice roll or get and set the img attribute `img/png-${number}`
+    IF dice roll !== 1 dice roll add to current score
+    Add current score to total score
+    Display total score.
+        IF dice roll === 1 Switch Player
+    
+
+    User holds the score (button)
+    Add current score to total score
+    IF total score === 100 Player wins
+        IF total score < 100 Switch Player
+    
+
+    User Resets the game
+    SET all scores to 0
+    SET player 1 as Starting player.
+
+The game is played by two players or you can play it it by your self to test.
--- script.js
+++ script.js
@@ -1,27 +1,5 @@
 'use strict';
 
-/* SET current score to 0 
-   SET Total score to 0
-    User roll the dice (button)
-    SET number generate numbers 1 - 6
-    Display dice roll or get and set the img attribute `img/png-${number}`
-    IF dice roll !== 1 dice roll add to current score
-    Add current score to total score
-    Display total score.
-        IF dice roll === 1 Switch Player
-    
-
-    User holds the score (button)
-    Add current score to total score
-    IF total score === 100 Player wins
-        IF total score < 100 Switch Player
-    
-
-    User Resets the game
-    SET all scores to 0
-    SET player 1 as Starting player.
-      */
-
 // Starting implementation
 let winnerState = document.querySelector('.winner--state');
 let player0EL = document.querySelector('.player--0');
